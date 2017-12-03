3d0012d02b1598da30f828d4cc907bf70a9362be
JCamiloB21 <32931153+JCamiloB21@users.noreply.github.com>
2017-12-03 11:39:27 -0500

    CurvaRotacion.c

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -10,7 +10,9 @@
 #define Mgal 2.325*10**7
 #define G 1
 double radio[301];
-double velocidad[301];
+double vel_obs[301];
+double chi;
+int i;
 int main(void){
     FILE *in;
     int i;
@@ -29,10 +31,17 @@ int main(void){
     for (i=1;i<301;i++){
         fscanf(in,"%lf %lf \n", &var1, &var2);
         radio[i]= var1;
-        velocidad[i]=var2;
+        vel_obs[i]=var2;
     }
     fclose(in);
 }
-
+float likelihood(double vel_model){
+    double suma=0;
+    for(i=0;i<301;i++){
+        suma= suma + (vel_obs[i]-vel_model)*(vel_obs[i]-vel_model);
+    }
+    double chi=(1.0/2.0)*suma;
+    return exp(-chi);
+}
 
 
