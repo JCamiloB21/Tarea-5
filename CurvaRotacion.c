dc89b23e664d02cb9e03c0fd01f21aeb8ff0f639
JCamiloB21 <32931153+JCamiloB21@users.noreply.github.com>
2017-12-03 22:23:58 -0500

    CurvaRotacion.c

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <string.h>
 
+
 #define bb 0.2497
 #define bd 5.16
 #define ad 0.3105
@@ -13,19 +14,16 @@
 double chi;
 int i;
 double sqrt(double x);
-double mb_walk[0];
-double md_walk[0];
-double mh_walk[0];
-double l_walk[0];
 double radio[301];
 double vel_obs[301];
-double likelihood(double hola[], double vel_model);
-double funcion(double chao[],double mb, double md, double mh);
+double likelihood(double hola[300], double vel_model[300]);
+double funcion(double chao[300],double mb, double md, double mh);
 void caminata();
+double randn (double mu, double sigma);
 
 int main(void){
    
-    double vel_model;
+    double vel_model[300];
     double mb;
     double md;
     double mh;
@@ -34,7 +32,7 @@ int main(void){
     double var1;
     double var2;
     int test;
-    char filename[150]="radialVelocities.dat";
+    char filename[150]="RadialVelocities.dat";
     printf("writing to file: %s\n",filename);
 
     in=fopen(filename,"r");
@@ -52,29 +50,63 @@ int main(void){
     likelihood(vel_obs,vel_model);
     caminata();
 }
-double likelihood(double hola[],double vel_model){
+double likelihood(double hola[300],double vel_model[300]){
     double suma=0;
     for(i=0;i<301;i++){
-        suma= suma + pow((hola[i]-vel_model),2);
+        suma= suma + pow((hola[i]-vel_model[i]),2);
     }
     double chi=(1.0/2.0)*suma;
     return exp(-chi);
 }
 
-double funcion(double chao[],double mb, double md, double mh){
+double funcion(double chao[300],double mb, double md, double mh){
     double vel;
     for(i=0;i<301;i++){
         vel= (sqrt(mb)*chao[i])/pow((chao[i]*chao[i] + bb*bb),3/4) + (sqrt(md)*chao[i])/pow((chao[i]*chao[i] + ((bd+ad)*(bd+ad))),3/4) + (sqrt(mh))/pow((chao[i]*chao[i] + ah*ah),3/4);
     }
     return vel;
 }
+double randn (double mu, double sigma)
+{
+  double U1, U2, W, mult;
+  static double X1, X2;
+  static int call = 0;
+ 
+  if (call == 1)
+    {
+      call = !call;
+      return (mu + sigma * (double) X2);
+    }
+ 
+  do
+    {
+      U1 = -1 + ((double) rand () / RAND_MAX) * 2;
+      U2 = -1 + ((double) rand () / RAND_MAX) * 2;
+      W = pow (U1, 2) + pow (U2, 2);
+    }
+  while (W >= 1 || W == 0);
+ 
+  mult = sqrt ((-2 * log (W)) / W);
+  X1 = U1 * mult;
+  X2 = U2 * mult;
+ 
+  call = !call;
+ 
+  return (mu + sigma * (double) X1);
+}
+/*el metodo gaussiano se tomo de la pagina https://phoxis.org/2013/05/04/generating-random-numbers-from-normal-distribution-in-c/*/
 void caminata(){
+    int n_interacciones=2000;
     double suma=0;
-    double y_init;
+    double *y_init=malloc(n_interacciones*sizeof(double));
     double mb_prime;
     double md_prime;
     double mh_prime;
-    double y_prime;
+    double *y_prime=malloc(n_interacciones*sizeof(double));
+    double *l_walk=malloc(n_interacciones*sizeof(double));
+    double *mb_walk=malloc(n_interacciones*sizeof(double));
+    double *md_walk=malloc(n_interacciones*sizeof(double));
+    double *mh_walk=malloc(n_interacciones*sizeof(double));
     double l_init;
     double l_prime;
     double alpha;
@@ -84,16 +116,15 @@ void caminata(){
     mb_walk[0]= drand48();
     md_walk[0]= drand48();
     mh_walk[0]= drand48();
-    y_init= funcion(radio,mb_walk[0],md_walk[0],mh_walk[0]);
+    y_init[i]= funcion(radio,mb_walk[0],md_walk[0],mh_walk[0]);
     l_walk[0]=likelihood(vel_obs,y_init);
-    int n_interacciones=2000;
     for(i=1;i<n_interacciones;i++){
-        mb_prime=drand48();
-        md_prime=drand48();
-        mh_prime=drand48(); 
+        mb_prime=randn(mb_walk[i], 0.1);
+        md_prime=randn(md_walk[i], 0.1);
+        mh_prime=randn(mh_walk[i], 0.1); 
         
-        y_init = funcion(radio, mb_walk[i],md_walk[i],mh_walk[i]);
-        y_prime = funcion(radio, mb_prime, md_prime, mh_prime);
+        y_init[i] = funcion(radio,mb_walk[i],md_walk[i],mh_walk[i]);
+        y_prime[i] = funcion(radio, mb_prime, md_prime, mh_prime);
         
         l_prime = likelihood(vel_obs, y_prime);
         l_init = likelihood(vel_obs, y_init);
@@ -139,7 +170,6 @@ void caminata(){
     printf("%lf\n",mejor_mb);
     printf("%lf\n",mejor_md);
     printf("%lf\n",mejor_mh);
-    
 }
 
 
